43d2c0674c71d8777f61a32c9a4bbb76f05e1f3e
Roland-Hufnagel <roland.hufnagel@gmx.de>
2023-04-17 16:42:36 +0200

    finish

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+import useSWR from "swr";
+const fetcher = (url) => fetch(url).then((r) => r.json());
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, error, isLoading } = useSWR(`/api/random-character`, fetcher);
+
+  if (error) {
+    return <p>an error occured</p>;
+  }
+  if (isLoading) {
+    return <p>is Loading...</p>;
+  }
+  return (
+    <>
+      <h1>
+        Hi, my name is {data.firstName} {data.lastName}.
+      </h1>
+      <p>My geohash is: {data.geohash}</p>
+      <p>Here is my twitter name: {data.twitterName}</p>
+      <p>Refresh the page to get next character...</p>
+    </>
+  );
 }
